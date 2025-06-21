589307f22734cde22e6138311eaf073efb4eb710
Mariyan Yordanov <marsrewq@gmail.com>
2025-06-21 13:06:13 +0300

    fix formData + sessionData

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,25 +7,34 @@ import { setSuccessMessage, setErrorMessage } from "../middlewares/tempDataMiddl
 const authController = Router();
 
 authController.get("/register", isGuest, (req, res) => {
-    res.render("auth/register", { pageTitle: "Register" });
+    const formData = req.session.formData || {};
+    delete req.session.formData;
+
+    res.render("auth/register", {
+        pageTitle: "Register",
+        email: formData.email
+    });
 });
 
 authController.post("/register", isGuest, async (req, res) => {
     const { email, password, rePassword } = req.body;
 
     if (!email || !password || !rePassword) {
+        req.session.formData = { email };
         setErrorMessage(req, "All fields are required");
         return res.redirect("/auth/register");
     }
 
     try {
         const token = await authService.register(email, password, rePassword);
+        delete req.session.formData;
         res.cookie(process.env.COOKIE_NAME, token);
 
         setSuccessMessage(req, "Registration successful! Welcome!");
         res.redirect("/");
 
     } catch (err) {
+        req.session.formData = { email };
         const errorMessage = getErrorMessage(err);
         setErrorMessage(req, errorMessage);
         res.redirect("/auth/register");
@@ -33,24 +42,33 @@ authController.post("/register", isGuest, async (req, res) => {
 });
 
 authController.get("/login", isGuest, (req, res) => {
-    res.render("auth/login", { pageTitle: "Login" });
+    const formData = req.session.formData || {};
+    delete req.session.formData;
+
+    res.render("auth/login", {
+        pageTitle: "Login",
+        email: formData.email
+    });
 });
 
 authController.post("/login", isGuest, async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) {
+        req.session.formData = { email };
         setErrorMessage(req, "All fields are required");
         return res.redirect("/auth/login");
     }
 
     try {
         const token = await authService.login(email, password);
+        delete req.session.formData; 
         res.cookie(process.env.COOKIE_NAME, token);
 
         setSuccessMessage(req, "Login successful!");
         res.redirect("/");
     } catch (err) {
+        req.session.formData = { email };
         const errorMessage = getErrorMessage(err);
         setErrorMessage(req, errorMessage);
         res.redirect("/auth/login");
